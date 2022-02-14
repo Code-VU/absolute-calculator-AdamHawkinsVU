f223d9ce4528dceca17f93227acbf55df78e8c73
Adam Hawkins <Adam.Hawkins@vu.com>
2022-02-14 08:12:54 -0600

    First draft

--- oddAbsolute.py
+++ oddAbsolute.py
@@ -2,10 +2,21 @@ def calculateAbsolute():
     
     # This first line is provided for you
     in_num  = input("Enter a number: ")
+    
+    try:
+        set_num = int(in_num)
+        if set_num <= 21:
+            smaller_abs_diff = abs(set_num - 21)
+            print (smaller_abs_diff)
+        elif set_num > 21:
+            larger_abs_diff = abs((set_num - 21) * 2)
+            print (larger_abs_diff)
+    except:
+        print ("Error: Please input a Number.")
     # end assignment
 
 ## if you want to test locally before you try to sync
 ## uncomment calculateAbsolute() and run > python payCalculator.py
 ## ***IMPORTANT*** please recomment before you submit/sync your assignment.
 ## OR YOUR TEST WILL NOT RUN
-# calculateAbsolute()
+#calculateAbsolute()
